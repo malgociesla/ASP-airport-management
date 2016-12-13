b91a8654c480b61220ee6963e466dadb692f3748
malgociesla <malgociesla@gmail.com>
2016-12-13 15:01:07 +0100

    iterating from start do end date by one day

--- airplane/GenerateSchedule.sql
+++ airplane/GenerateSchedule.sql
@@ -1,8 +1,26 @@
 ﻿CREATE PROCEDURE [dbo].[GenerateSchedule]
-	@dateFrom date,
-	@dateTo date,
+	@startDate date,
+	@endDate date,
 	@flightId uniqueidentifier =NULL
 AS
-	--SELECT @dateFrom, @dateTo
-	select idFlight from [dbo].[Arrival] a inner join [dbo].[Flight] f on a.idArrival=f.idArrival where arrivalTime>@dateTo;
+	WHILE (@startDate < @endDate)
+	BEGIN
+	SET @startDate=DATEADD(day,1,@startDate); 
+	PRINT @startDate
+	END
+
 RETURN 0
+
+	--select DATEADD(day,1,'2006-12-13');
+
+	--SELECT @dateFrom, @dateTo
+	--select idFlight from [dbo].[Arrival] a inner join [dbo].[Flight] f on a.idArrival=f.idArrival where arrivalTime>@dateTo;
+
+	--today day no
+	--SELECT DATEPART(dw,GETDATE());
+	--today day name
+	--SELECT DATENAME(dw,GETDATE());
+
+	--EXECUTE
+	--execute [dbo].[GenerateSchedule] @startDate='2006-12-13', @endDate='2006-12-28';
+
