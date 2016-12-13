cc2c8f434d2e4791f15e3ab3cf23ecf701e943e5
malgociesla <malgociesla@gmail.com>
2016-12-13 13:07:41 +0100

    Changed SSDT version

---
+++ airplane/GenerateSchedule.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[GenerateSchedule]
+	@dateFrom date,
+	@dateTo date,
+	@flightId uniqueidentifier =NULL
+AS
+	--SELECT @dateFrom, @dateTo
+	select idFlight from [dbo].[Arrival] a inner join [dbo].[Flight] f on a.idArrival=f.idArrival where arrivalTime>@dateTo;
+RETURN 0
