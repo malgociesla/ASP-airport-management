5d31b59d7db029f783fb3a968e5708b955deebcb
malgociesla <malgociesla@gmail.com>
2016-12-13 13:42:20 +0100

    arrivalTime changed to time

--- airplane/dbo/Tables/Arrival.sql
+++ airplane/dbo/Tables/Arrival.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Arrival] (
     [idArrival]   UNIQUEIDENTIFIER NOT NULL,
     [idCity]      UNIQUEIDENTIFIER NULL,
-    [arrivalTime] TIME (7)         NULL,
+    [arrivalTime] TIME         NULL,
     PRIMARY KEY CLUSTERED ([idArrival] ASC),
     FOREIGN KEY ([idCity]) REFERENCES [dbo].[City] ([idCity])
 );
