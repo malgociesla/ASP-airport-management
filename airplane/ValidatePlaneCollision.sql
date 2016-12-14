0fb5416914fec8be87ebbb1e7283a70803df9b7b
malgociesla <malgociesla@gmail.com>
2016-12-14 17:01:11 +0100

    check how many planes are landing at the same time

--- airplane/ValidatePlaneCollision.sql
+++ airplane/ValidatePlaneCollision.sql
@@ -2,14 +2,27 @@
 --max 4 planes landing at the same time
 --when collision occures, find free time (iterate by 5 minutes)
 CREATE TRIGGER [dbo].[ValidatePlaneCollision]
-ON Schedule
+ON [dbo].[Schedule]
 INSTEAD OF INSERT, UPDATE
 AS
 BEGIN
-
 	SET NOCOUNT ON
+	DECLARE @arrivalDT datetime;
+	DECLARE @landingTime time;
+	--get inserted arrival datetime
+	SELECT @arrivalDT= (select arrivalDT from inserted)
+	--convert arrival datetime to landing time
+	SET @landingTime= cast(@arrivalDT as time);
+	--check how many planes are landing at the same time
 	DECLARE @result int;
-	exec @result = [dbo].[GetCountOfLandingPlanes] @landingTime='18:00:00.0000000';
-	print @result;
+	exec @result = [dbo].[GetCountOfLandingPlanes] @landingTime=@landingTime;
+	--if there are more than 4 planes landing at the same time
+	IF (@result>4)
+		BEGIN
+		print 'more than 4 planes landing'
+		END
+		ELSE
+		print 'no collision'
+		--here just insert/update data
 
 END
