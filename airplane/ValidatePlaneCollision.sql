41faae935fbba08443fbd5c295126247b4d4146d
malgociesla <malgociesla@gmail.com>
2016-12-14 16:40:31 +0100

    trigger on schema table - check for collision

---
+++ airplane/ValidatePlaneCollision.sql
@@ -0,0 +1,15 @@
+﻿--validate plane collision
+--max 4 planes landing at the same time
+--when collision occures, find free time (iterate by 5 minutes)
+CREATE TRIGGER [dbo].[ValidatePlaneCollision]
+ON Schedule
+INSTEAD OF INSERT, UPDATE
+AS
+BEGIN
+
+	SET NOCOUNT ON
+	DECLARE @result int;
+	exec @result = [dbo].[GetCountOfLandingPlanes] @landingTime='18:00:00.0000000';
+	print @result;
+
+END
