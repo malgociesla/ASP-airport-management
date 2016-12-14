e03bf97354bd583061d707416aa18eb4ced35d26
malgociesla <malgociesla@gmail.com>
2016-12-14 15:44:38 +0100

    function counting planes landing at the same time

---
+++ airplane/GetCountOfLandingPlanes.sql
@@ -0,0 +1,12 @@
+﻿CREATE FUNCTION [dbo].[GetCountOfLandingPlanes]
+(
+	@landingTime time
+)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @result int;
+	--should we chceck arrival location?
+	SET @result=(select count(arrivalTime) from Flight f inner join Arrival a on a.idArrival=f.idArrival where @landingTime=arrivalTime);
+	RETURN @result;
+END
