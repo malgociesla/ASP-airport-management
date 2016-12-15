e271e3862044b42aa1c910846c3aef65fa4253c6
malgociesla <malgociesla@gmail.com>
2016-12-15 15:05:07 +0100

    checking for collision works todo: check if it's ok

--- airplane/dbo/Triggers/ValidatePlaneCollision.sql
+++ airplane/dbo/Triggers/ValidatePlaneCollision.sql
@@ -1,37 +1,36 @@
-﻿--validate plane collision
---@maxLandingCapacity - change for how many planes can land at the same time
---when collision occures, find free time (iterate by 5 minutes)
-
-CREATE TRIGGER [dbo].[ValidatePlaneCollision]
+﻿CREATE TRIGGER [dbo].[ValidatePlaneCollision]
 ON [dbo].[Schedule]
 INSTEAD OF INSERT, UPDATE
 AS
 BEGIN
 	SET NOCOUNT ON
 	--max 4 planes landing at the same time
-	DECLARE @maxLandingCapacity INT = 4
+	DECLARE @maxLandingCapacity INT = 1
 	DECLARE @arrivalDT DATETIME
 	--get inserted arrival datetime
 	SELECT @arrivalDT= (SELECT arrivalDT
 						FROM inserted)
 
+	DECLARE @timeCounter DATETIME = @arrivalDT
 	--check how many planes are landing at the same time
-	DECLARE @planeCount INT = @maxLandingCapacity +1	
+	DECLARE @planeCount INT	
+	EXEC @planeCount = [dbo].[GetCountOfLandingPlanes] @landingDT=@timeCounter
 
 	--if there are to many planes landing at the same time
-	DECLARE @timeCounter DATETIME = @arrivalDT
-	WHILE (@planeCount>@maxLandingCapacity)
+	IF (@planeCount>@maxLandingCapacity)
 	BEGIN
-		PRINT 'too many planes landing'
 		SET @timeCounter = DATEADD(MINUTE,5,@timeCounter)
-		EXEC @planeCount = [dbo].[GetCountOfLandingPlanes] @landingDT=@timeCounter
+		--insert data
+		--if we changed arrival time due to collision -> insert @timeCounter insted of arrivalDT
+		INSERT INTO [dbo].[Schedule]
+		SELECT i.idSchedule,i.idFlight,i.departureDT,@timeCounter
+		FROM inserted AS i
 	END
-	--insert data, but change inserted.arrivalDT to @timeCounter
-	--INSERT INTO  [dbo].[Schedule] (idSchedule,idFlight,departureDT,arrivalDT)
-	--inserted.
-	--FROM inserted
-
-	INSERT INTO [dbo].[Schedule]
-	SELECT *
-	FROM inserted
+	--if planes can land
+	ELSE
+	BEGIN
+		INSERT INTO [dbo].[Schedule]
+		SELECT *
+		FROM inserted
+	END
 END
