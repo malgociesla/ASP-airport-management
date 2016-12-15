e3c7d5dc5744f39f4668f8f89100b267839c4fe7
malgociesla <malgociesla@gmail.com>
2016-12-15 15:27:52 +0100

    works better TODO iteration step should be 5min instead of 10min

--- airplane/dbo/Triggers/ValidatePlaneCollision.sql
+++ airplane/dbo/Triggers/ValidatePlaneCollision.sql
@@ -5,7 +5,7 @@ AS
 BEGIN
 	SET NOCOUNT ON
 	--max 4 planes landing at the same time
-	DECLARE @maxLandingCapacity INT = 1
+	DECLARE @maxLandingCapacity INT = 4
 	DECLARE @arrivalDT DATETIME
 	--get inserted arrival datetime
 	SELECT @arrivalDT= (SELECT arrivalDT
@@ -17,20 +17,19 @@ BEGIN
 	EXEC @planeCount = [dbo].[GetCountOfLandingPlanes] @landingDT=@timeCounter
 
 	--if there are to many planes landing at the same time
-	IF (@planeCount>@maxLandingCapacity)
+	WHILE (@planeCount>@maxLandingCapacity)
 	BEGIN
+	PRINT 'too many'
+	PRINT @timeCounter
 		SET @timeCounter = DATEADD(MINUTE,5,@timeCounter)
+		EXEC @planeCount = [dbo].[GetCountOfLandingPlanes] @landingDT=@timeCounter
+		PRINT @timeCounter
+	END
+	--if planes can land
 		--insert data
 		--if we changed arrival time due to collision -> insert @timeCounter insted of arrivalDT
 		INSERT INTO [dbo].[Schedule]
 		SELECT i.idSchedule,i.idFlight,i.departureDT,@timeCounter
 		FROM inserted AS i
-	END
-	--if planes can land
-	ELSE
-	BEGIN
-		INSERT INTO [dbo].[Schedule]
-		SELECT *
-		FROM inserted
-	END
+	
 END
