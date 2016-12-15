5f69f9c9bc52f0ed1b2472f95f81a73324f35c89
malgociesla <malgociesla@gmail.com>
2016-12-15 13:39:45 +0100

    available landing time iteration todo: insert/update with modified arrival time

--- airplane/dbo/Triggers/ValidatePlaneCollision.sql
+++ airplane/dbo/Triggers/ValidatePlaneCollision.sql
@@ -1,5 +1,5 @@
 ﻿--validate plane collision
---max 4 planes landing at the same time
+--@maxLandingCapacity - change for how many planes can land at the same time
 --when collision occures, find free time (iterate by 5 minutes)
 
 CREATE TRIGGER [dbo].[ValidatePlaneCollision]
@@ -8,55 +8,30 @@ INSTEAD OF INSERT, UPDATE
 AS
 BEGIN
 	SET NOCOUNT ON
-	DECLARE @arrivalDT DATETIME;
-	DECLARE @arrivalTime TIME;
+	--max 4 planes landing at the same time
+	DECLARE @maxLandingCapacity INT = 4
+	DECLARE @arrivalDT DATETIME
 	--get inserted arrival datetime
 	SELECT @arrivalDT= (SELECT arrivalDT
 						FROM inserted)
-	--convert arrival datetime to landing time
-	SET @arrivalTime= CAST(@arrivalDT AS TIME);
-	--check how many planes are landing at the same time
-	DECLARE @result INT;
-	EXEC @result = [dbo].[GetCountOfLandingPlanes] @landingTime=@arrivalTime;
-	--if there are more than 4 planes landing at the same time
-	IF (@result>4)
-		BEGIN
-		PRINT 'more than 4 planes landing'
-		--select (n-4) planes that must look for free landing time
-		--push (n-4) planes to cursor
-		DECLARE @providedDT DATETIME;
-		DECLARE @collidedPlanesCursor CURSOR;
-		SET @collidedPlanesCursor = CURSOR FOR
-									SELECT @arrivalDT
-									FROM [dbo].[Schedule] s
-									WHERE s.arrivalDT=s.arrivalDT
-		OPEN @collidedPlanesCursor
-		FETCH NEXT FROM @collidedPlanesCursor 
-		INTO @thisIdFlight
-		WHILE @@FETCH_STATUS = 0
-			BEGIN
-			
 
-			
-				SET @timeCounter = @providedTime;
-				--for each of them search for free landing time
-				SET @timeCounter = DATEADD(MINUTES,5,@providedDT)
-				--insert data with changed landingTime
-				--cursor
-				FETCH NEXT FROM @collidedPlanesCursor
-				INTO @thisIdFlight 
-			END; 
-		--clean cursor
-		CLOSE @collidedPlanesCursor;
-		DEALLOCATE @collidedPlanesCursor;
-		END
-	ELSE
-		BEGIN
-		PRINT 'no collision'
-		--here just insert/update data
-		END
-	--for now insert data anyway
+	--check how many planes are landing at the same time
+	DECLARE @planeCount INT = @maxLandingCapacity +1	
+
+	--if there are to many planes landing at the same time
+	DECLARE @timeCounter DATETIME = @arrivalDT
+	WHILE (@planeCount>@maxLandingCapacity)
+	BEGIN
+		PRINT 'too many planes landing'
+		SET @timeCounter = DATEADD(MINUTE,5,@timeCounter)
+		EXEC @planeCount = [dbo].[GetCountOfLandingPlanes] @landingDT=@timeCounter
+	END
+	--insert data, but change inserted.arrivalDT to @timeCounter
+	--INSERT INTO  [dbo].[Schedule] (idSchedule,idFlight,departureDT,arrivalDT)
+	--inserted.
+	--FROM inserted
+
 	INSERT INTO [dbo].[Schedule]
 	SELECT *
-	FROM inserted;
+	FROM inserted
 END
