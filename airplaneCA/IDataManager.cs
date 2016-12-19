be8d1dc36ee93217702a492aba87c91d590a4dd9
malgociesla <malgociesla@gmail.com>
2016-12-19 13:07:29 +0100

    IDataManager.cs

---
+++ airplaneCA/IDataManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace airplaneCA
+{
+    interface IDataManager
+    {
+        void SetDataManager(IConnection conn);
+        void Close();
+        void Print();
+        void PrintTable(string tableName);
+        void GenerateSchedule(string startDate, string endDate);
+    }
+}
