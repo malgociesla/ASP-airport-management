0d67e4374310198bd5816a3847440d7e1ee7dee0
malgociesla <malgociesla@gmail.com>
2016-12-21 13:01:17 +0100

    ASP app - display cities

---
+++ AirplaneASP/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AirportService;
+using AirportService.DTO;
+
+namespace AirplaneASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ICityService cityService= new CityService();
+            List<CityDTO> cityList = cityService.GetCities();
+            ViewBag.CityList = cityList;
+            ViewBag.CityService = cityService;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
