1ed7b52ec7e6e76a3718ad0158a97b88353cb820
malgociesla <malgociesla@gmail.com>
2016-12-22 16:01:19 +0100

    new classes for services

---
+++ AirportService/FlightService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirportService.DTO;
+using AirplaneEF;
+
+namespace AirportService
+{
+    public class FlightService : IFlightService
+    {
+    }
+}
