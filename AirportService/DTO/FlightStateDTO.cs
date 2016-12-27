4c9037fed3e6950e634e715db233a4d4e4474259
malgociesla <malgociesla@gmail.com>
2016-12-27 10:51:45 +0100

    FlightState instead of FStatus table

---
+++ AirportService/DTO/FlightStateDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AirportService.DTO
+{
+    public class FlightStateDTO
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ airplane/dbo/Tables/FlightState.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[FlightState] (
+    [idFlightState] UNIQUEIDENTIFIER DEFAULT (newsequentialid()) NOT NULL,
+    [name]      VARCHAR (30)     NULL,
+    PRIMARY KEY CLUSTERED ([idFlightState] ASC)
+);
+
