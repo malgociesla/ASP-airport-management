e956a7a6e5223584be90ef77aa12996d7a7a802e
malgociesla <malgociesla@gmail.com>
2016-12-28 11:51:47 +0100

    Companies List() Add() Remove()

---
+++ AirplaneASP/Controllers/CompaniesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AirportService;
+using AirportService.DTO;
+
+namespace AirplaneASP.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        [HttpGet]
+        public ActionResult List()
+        {
+            ICompanyService companyService = new CompanyService();
+            List<CompanyDTO> companyList = companyService.GetAll();
+
+            return View("List", companyList);
+        }
+
+        [HttpGet]
+        public ActionResult Remove(Guid companyID)
+        {
+            ICompanyService companyService = new CompanyService();
+            companyService.Remove(companyID);
+
+            return List();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(CompanyDTO company)
+        {
+            ICompanyService companyService = new CompanyService();
+            companyService.Add(company);
+            return List();
+        }
+    }
+}
