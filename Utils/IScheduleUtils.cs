e6f0e43fb2d9c67d1b84677c1e63983537ad678c
malgociesla <malgociesla@gmail.com>
2017-01-27 18:03:46 +0100

    added missing file

---
+++ Utils/IScheduleUtils.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Utils
+{
+    public interface IScheduleUtils
+    {
+        //List<List<Dictionary<string, int>>> Read(Stream excelStream);
+        byte[] Write(List<List<Dictionary<string, int>>> schedulesList);
+    }
+}
