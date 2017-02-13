b345600f6182016adba1b6b52ac91dd85aa12288
malgociesla <malgociesla@gmail.com>
2017-02-13 17:58:13 +0100

    Cutom Exception for Utils

---
+++ Utils/UtilsException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Utils
+{
+    [Serializable]
+    public class UtilsException : ApplicationException
+    {
+        public UtilsException() { }
+        public UtilsException(string message) : base(message) { }
+        public UtilsException(string message, Exception inner) : base(message, inner) { }
+        protected UtilsException(System.Runtime.Serialization.SerializationInfo info,
+                                 System.Runtime.Serialization.StreamingContext context)
+                                : base(info, context) { }
+}
+}
