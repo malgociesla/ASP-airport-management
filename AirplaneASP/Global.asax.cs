79b17bd48449ba315f63a104039e3d99f9bff894
malgociesla <malgociesla@gmail.com>
2017-02-15 14:11:54 +0100

    DI for ExceptionLogger

--- AirplaneASP/Global.asax.cs
+++ AirplaneASP/Global.asax.cs
@@ -3,17 +3,13 @@ using AirplaneASP.Loggers;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web;
 
 namespace AirplaneASP
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
-        private readonly IExceptionLogger _exceptionLogger;
-        MvcApplication() {  }
-        MvcApplication(IExceptionLogger exceptionLogger)
-        {
-            this._exceptionLogger = exceptionLogger;
-        }
+        private readonly IExceptionLogger _exceptionLogger = DependencyResolver.Current.GetService<IExceptionLogger>();
 
         protected void Application_Start()
         {
@@ -30,7 +26,7 @@ namespace AirplaneASP
             //Logg exception
             _exceptionLogger.LogException(raisedException);
 
-            // Process exception
+            //Process exception
             Server.ClearError();
             Response.Redirect("/Shared/Error");
         }
--- AirplaneASP/Loggers/ExceptionLogger.cs
+++ AirplaneASP/Loggers/ExceptionLogger.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Configuration;
-using System.Web.Mvc;
 
 namespace AirplaneASP.Loggers
 {
