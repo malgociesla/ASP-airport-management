eb83680255ded537b2bc781dd7050bb23b1d0685
malgociesla <malgociesla@gmail.com>
2017-02-23 11:10:11 +0100

    User's schedule table in progress

--- AirplaneASP/Controllers/UserController.cs
+++ AirplaneASP/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using AirplaneASP.Mapping;
 using AirplaneASP.Models.Cities;
 using AirplaneASP.Models.Countries;
+using AirplaneASP.Models.Schedules;
 using AirportService;
 using AirportService.DTO;
 using System;
@@ -17,19 +18,26 @@ namespace AirplaneASP.Controllers
     {
         private readonly ICityService _cityService;
         private readonly ICountryService _countryService;
+       private readonly IScheduleService _scheduleService;
 
         private readonly IMapper<CityDTO, CityModel> _cityMaper;
         private readonly IMapper<CountryDTO, CountryModel> _countryMaper;
+        private readonly IMapper<ScheduleDetailsDTO, ScheduleDetailsImportModel> _scheduleDetailsMaper;
+
         public UserController(ICityService cityService,
                                 ICountryService countryService,
+                                IScheduleService scheduleService,
 
                                 IMapper<CityDTO, CityModel> cityMaper,
-                                IMapper<CountryDTO, CountryModel> countryMaper)
+                                IMapper<CountryDTO, CountryModel> countryMaper,
+                                IMapper<ScheduleDetailsDTO, ScheduleDetailsImportModel> scheduleDetailsMaper)
         {
             this._cityService = cityService;
             this._countryService = countryService;
+            this._scheduleService = scheduleService;
             this._cityMaper = cityMaper;
             this._countryMaper = countryMaper;
+            this._scheduleDetailsMaper = scheduleDetailsMaper;
         }
         public ActionResult Index()
         {
@@ -47,6 +55,17 @@ namespace AirplaneASP.Controllers
             var cityDTOList = _cityService.GetAll();
             var cityList = _cityMaper.Map(cityDTOList);
             return Json(cityList, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult GetSchedule()
+        {
+            int totalItemsCount = 0;
+            //List<ScheduleDetailsDTO> scheduleDTOPage = _scheduleService.GetList(pageNumber, pageSize, out totalItemsCount, from, to);
+            List<ScheduleDetailsDTO> scheduleDTOPage = _scheduleService.GetList(1, 12, out totalItemsCount);
+            //get subset of IPagedList and translate from ScheduleDetailsDTO to ScheduleDetailsModel
+            var subset = _scheduleDetailsMaper.Map(scheduleDTOPage);
+            return Json(subset, JsonRequestBehavior.AllowGet);
         }
     }
 }
